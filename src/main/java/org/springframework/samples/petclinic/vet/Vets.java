42b0407b38e63ac71a576739bc147faa95cdd754
PaulW02 <102044676+PaulW02@users.noreply.github.com>
2025-03-28 12:37:35 +0000

    Bugfix

--- src/main/java/org/springframework/samples/petclinic/vet/Vets.java
+++ src/main/java/org/springframework/samples/petclinic/vet/Vets.java
@@ -22,7 +22,7 @@ import jakarta.xml.bind.annotation.XmlElement;
 import jakarta.xml.bind.annotation.XmlRootElement;
 
 /**
- * Simple domain object representing a list of veterinarians. Mostly to be used for
+ * Simple domain object representing a list of veterinarians. Mostly here to be used for
  * the 'vets' {@link org.springframework.web.servlet.view.xml.MarshallingView}.
  *
  * @author Arjen Poutsma
